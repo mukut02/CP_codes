93c9f1f8593fa5e39f060a65ca49b6d43b248823
mukut02 <chatterjeemukut@gmail.com>
2025-07-30 13:37:32 +0530

    Create base1.cpp

---
+++ base1.cpp
@@ -0,0 +1,21 @@
+ #include<bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+void solve(){
+    
+}
+ 
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;cin >> t;
+    while(t--){
+        solve();
+     }
+}
+// <bits/stdc++.h> contains all C++STL fxns
+//all 64 bit integer no worry for overflow
+//ios::sync part for fast read write saves from TLE
